92f0a0e5b88091e8977a66a106b7509624c2d770
hapax <jahzielv@gmail.com>
2023-04-02 22:15:03 -0400

    feat: init

---
+++ README.md
@@ -0,0 +1,10 @@
+# SilverBullet plug for food tracking
+
+Track your food in SilverBullet!
+
+## Wait, SilverBullet?
+
+If you don't know what it is, check its [webpage](https://silverbullet.md), but
+if you want me to spoil the fun: it is an extensible note taking app with
+markdown and plain files at its core (well... there is a bit of magic in there
+too, but what good it would be without a little magic?)
---
+++ food.ts
@@ -0,0 +1,26 @@
+import { editor, markdown } from "$sb/silverbullet-syscall/mod.ts";
+
+const foodEmoji = ["🥔", "🌶️", "🥝", "🥨", "🥞", "🍖", "🍜", "🍞", "🍱"];
+
+function randomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+export async function food() {
+  console.log("food gang");
+  const i = randomNumber(0, foodEmoji.length);
+  const e = foodEmoji[i];
+  const food = await editor.prompt(e + "enter some food ");
+  if (!food) {
+    return;
+  }
+
+  await editor.navigate("food test");
+  const mdTree = await markdown.parseMarkdown(await editor.getText());
+  const d = new Date();
+
+  await editor.insertAtPos(
+    `\n- _${d.toLocaleString()}_ ${food}`,
+    mdTree.to ?? 0
+  );
+}
